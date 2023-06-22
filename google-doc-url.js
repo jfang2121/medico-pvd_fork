32990b1b9a4a5e8357e7959e42bcb1f83045a2b2
mathprofcarrie <75792134+mathprofcarrie@users.noreply.github.com>
2023-06-22 09:49:53 -0400

    Update google-doc-url.js
    Added url

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,5 @@
-// // paste in your published Google Sheets URL from the browser address bar
+// Use the Google Sheets template to create the Google Sheet that you need.
+// Then Publish the Google Sheet as csv
+
+// paste in your published Google Sheets URL from the browser address bar
+https://docs.google.com/spreadsheets/d/e/2PACX-1vSo8zAjfZ0Ep8ShroN0-S-3J_g-P29un6MMW2rERBcTp8BSVEtIbrUKQceXuYWMfktOgA-iV1wadsb6/pub?output=csv 
